710b399cf29994b5cd7a3ff92d0fe7fcb3e25c5d
U-CCR\tli7 <tli7@tli7-MOBL1.ccr.corp.intel.com>
2019-02-25 21:32:20 +0800

    change icache to dcache
    Committer: Tianyou Li <tianyou.li@gmail.com>
    
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   ../branch/test.sh
    	modified:   ../dcache/test.sh

--- dcache/test.sh
+++ dcache/test.sh
@@ -13,8 +13,8 @@ cd ${BLD_DIR}
 
 g++ -O1 -std=c++11 -o test main.cc
 
-perf stat -B -e L1-icache-load-misses,instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses ./test 10000 0
-perf stat -B -e L1-icache-load-misses,instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses ./test 10000 1
+perf stat -B -e L1-icache-load-misses,instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-dcache-store-misses ./test 10000 0
+perf stat -B -e L1-icache-load-misses,instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-dcache-store-misses ./test 10000 1
 
 
 cd ${CUR_DIR}
